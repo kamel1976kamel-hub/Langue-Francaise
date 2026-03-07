eff2481726a3109143a73e8ff456d91679b6f409
kamel1976kamel-hub <chellouaikamel50@gmail.com>
2026-03-07 03:28:41 +0100

    change
    change

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./main.js", "./chat-system.js", "./activities.js", "./audio-system.js", "./ui-elements.js", "./utils.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
